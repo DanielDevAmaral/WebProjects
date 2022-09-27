2492e51c0541165c34aaa6bee731fb8f09686937
DanielDevAmaral <daniel.amaral1928@outlook.com>
2022-09-26 21:41:04 -0300

    Add my first website structure

---
+++ CSE110/Checkpoint/functions.py
@@ -0,0 +1,15 @@
+def display_regular(name):
+    print(name)
+
+def display_uppercase(name):
+    print(name.upper())
+
+def display_lowercase(name):
+    print(name.lower())
+
+user_name = input("What is your message? ")
+display_regular(user_name)
+display_lowercase(user_name)
+display_uppercase(user_name)
+
+
